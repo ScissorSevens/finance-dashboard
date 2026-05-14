7585e9b58169f972bd3b82cc43eb79a07c6e2b99
Felipe Bernal <felipeb.pachon@gmail.com>
2026-05-14 16:16:26 -0500

    fix: add base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import preact from '@astrojs/preact';
 
 export default defineConfig({
 	site: 'https://ScissorSevens.github.io',
+	base: '/finance-dashboard',
 	integrations: [
 		tailwind(),
 		preact({ compat: true }),
